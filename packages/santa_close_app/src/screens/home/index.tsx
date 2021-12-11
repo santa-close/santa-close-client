53f48e694fc2b0b17e2fed54b52d0b9ad4455a31
lifeisegg123 <leejj2002@naver.com>
2021-12-11 23:21:57 +0900

    구글 로그인 추가

--- packages/santa_close_app/src/screens/home/index.tsx
+++ packages/santa_close_app/src/screens/home/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import KakaoButton from '../..//test/KakaoButton';
+import GoogleButton from '../../test/GoogleButton';
+import KakaoButton from '../../test/KakaoButton';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>홈</Text>
-      <KakaoButton />
+      <View style={{justifyContent: 'space-around', height: '100%'}}>
+        <KakaoButton />
+        <GoogleButton />
+      </View>
     </View>
   );
 };
