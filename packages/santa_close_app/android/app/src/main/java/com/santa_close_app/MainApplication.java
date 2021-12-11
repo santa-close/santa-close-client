f3d8402f4e9b299efbf2509adc08c6f296ec2605
lifeisegg123 <leejj2002@naver.com>
2021-12-11 22:16:00 +0900

    app nohoist 추가

--- packages/santa_close_app/android/app/src/main/java/com/santa_close_app/MainApplication.java
+++ packages/santa_close_app/android/app/src/main/java/com/santa_close_app/MainApplication.java
@@ -31,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected String getJSMainModuleName() {
-          return "packages/santa_close_app/index";
+          return "index";
         }
       };
 
