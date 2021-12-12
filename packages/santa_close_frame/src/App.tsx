df3fe104d59fd77d8986713bc2e61f9d0db760b8
Seongcheol Jo <josworks27@gmail.com>
2021-12-12 19:47:38 +0900

    Add karrotframe
    * Frame App 최상위 컴포넌트에 캐롯프레임 적용

--- packages/santa_close_frame/src/App.tsx
+++ packages/santa_close_frame/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Navigator, Screen} from '@karrotframe/navigator'
 // import {Box} from 'santa_close_uikit'
 
 const MapApp = React.lazy(() => import('map-app/MapApp'))
 
-const App = () => {
+const Page1 = () => {
+  return <h1>{'Here is PAGE1'}</h1>
+}
+
+const Page2 = () => {
+  return <h1>{'Here is PAGE2'}</h1>
+}
+
+const MapAppContainer = () => {
   return (
     <div
       style={{
@@ -11,12 +20,21 @@ const App = () => {
         border: '1px solid blue',
       }}
     >
-      <h1>{'This is Frame App of santa-close-frame'}</h1>
-      <React.Suspense fallback={'Loading...'}>
+      <h1>{'This is Frame App of santa-close-frame with KarrotFrame'}</h1>
+      <React.Suspense fallback={'loading...'}>
         <MapApp />
       </React.Suspense>
-      {/*<Box />*/}
     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <Navigator>
+      <Screen component={MapAppContainer} path="/" />
+      <Screen component={Page1} path="/page1" />
+      <Screen component={Page2} path="/page2" />
+    </Navigator>
   )
 }
 
