96ecf0584ce0abdc883165cbbd30bb767d292179
Seongcheol Jo <josworks27@gmail.com>
2021-12-12 18:21:49 +0900

    Add MFE
    * frame & map app MFE 세팅

--- packages/santa_close_frame/src/App.tsx
+++ packages/santa_close_frame/src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 // import {Box} from 'santa_close_uikit'
 
-// import MapApp from 'map-app/MapApp'
-
 const MapApp = React.lazy(() => import('map-app/MapApp'))
 
 const App = () => {
   return (
-    <div>
-      <h1>{'This is Frame App'}</h1>
-      <MapApp />
+    <div
+      style={{
+        margin: '10px',
+        border: '1px solid blue',
+      }}
+    >
+      <h1>{'This is Frame App of santa-close-frame'}</h1>
+      <React.Suspense fallback={'Loading...'}>
+        <MapApp />
+      </React.Suspense>
       {/*<Box />*/}
     </div>
   )
--- packages/santa_close_uikit/src/components/Box/index.tsx
+++ packages/santa_close_uikit/src/components/Box/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Box = () => {
-  return <div>{'This is Box component'}</div>
+  return (
+    <div
+      style={{
+        margin: '10px',
+        border: '1px solid green',
+      }}
+    >
+      <h1>{'This is Box component of santa-close-uikit'}</h1>
+    </div>
+  )
 }
 
 export default Box
