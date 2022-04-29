b916acb45357c147287433f1abad61bfa757e700
lifeisegg123 <leejj2002@naver.com>
2022-04-30 02:38:09 +0900

    chore: setup common packages & add watch ds, common script

---
+++ packages/santa_close_common/vite.config.js
@@ -0,0 +1,19 @@
+const react = require('@vitejs/plugin-react')
+const path = require('path')
+const {defineConfig} = require('vite')
+
+module.exports = defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'common',
+      fileName: (format) => `index.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {},
+    },
+  },
+})
