9787f8c520b333b27ffa99c78eed32100cdcce23
EUNYUSEO <eyseo328@gmail.com>
2022-05-02 21:01:59 +0900

    fix: graphql lint 수정
    Co-authored-by: lifeisegg <lifeisegg123@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,13 @@
 const path = require('path')
 
 module.exports = {
-  plugins: ['react', 'prettier'],
-  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
   ignorePatterns: ['**/*.config.js'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      plugins: ['@typescript-eslint', 'prettier'],
-      extends: ['airbnb-typescript', 'prettier'],
-      parserOptions: {
-        project: ['./tsconfig.json', './packages/*/tsconfig.json'],
-        tsconfigRootDir: __dirname,
-      },
-      rules: {
-        'import/no-extraneous-dependencies': [
-          'error',
-          {
-            packageDir: [
-              __dirname,
-              path.resolve(__dirname, './packages/santa_close_app'),
-              path.resolve(__dirname, './packages/santa_close_map'),
-              path.resolve(__dirname, './packages/santa_close_frame'),
-              path.resolve(__dirname, './packages/santa_close_design-system'),
-              path.resolve(__dirname, './packages/santa_close_common'),
-            ],
-            devDependencies: [
-              '**/*.test.ts',
-              '**/*.spec.ts',
-              '**/*.setup.ts',
-              '**/*.test.tsx',
-              '**/*.spec.tsx',
-              '**/*.config.js',
-              '**/*.config.ts',
-            ],
-          },
-        ],
-      },
-    },
-    {
-      files: ['*.graphql'],
-      parser: '@graphql-eslint/eslint-plugin',
-      plugins: ['@graphql-eslint'],
-      extends: ['plugin:@graphql-eslint/operations-recommended'],
-      rules: {
-        '@graphql-eslint/executable-definitions': 'off',
-      },
-      parserOptions: {
-        schema: './packages/santa_close_common/schema.graphql',
-        operations: './packages/**/*.graphql',
-      },
-    },
-  ],
+  parserOptions: {
+    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
@@ -59,6 +15,28 @@ module.exports = {
     'react/function-component-definition': [
       2,
       {namedComponents: 'arrow-function'},
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: [
+          __dirname,
+          path.resolve(__dirname, './packages/santa_close_app'),
+          path.resolve(__dirname, './packages/santa_close_map'),
+          path.resolve(__dirname, './packages/santa_close_frame'),
+          path.resolve(__dirname, './packages/santa_close_design-system'),
+          path.resolve(__dirname, './packages/santa_close_common'),
+        ],
+        devDependencies: [
+          '**/*.test.ts',
+          '**/*.spec.ts',
+          '**/*.setup.ts',
+          '**/*.test.tsx',
+          '**/*.spec.tsx',
+          '**/*.config.js',
+          '**/*.config.ts',
+        ],
+      },
     ],
     'import/prefer-default-export': 'off',
     'arrow-body-style': ['off'],
---
+++ graphql.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  schema: ['http://localhost:4000/graphql'],
+  documents: ['**/*.{graphql,js,ts,jsx,tsx}'],
+}
