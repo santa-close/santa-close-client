ae53bc9e79b4b601053a1bd4a585bfbc5777e36d
lifeisegg123 <leejj2002@naver.com>
2022-05-08 21:02:01 +0900

    feat: bridge function & example

--- packages/santa_close_app/src/App.tsx
+++ packages/santa_close_app/src/App.tsx
@@ -1,35 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-
-import HomeScreen from './screens/home';
-import WebViewSampleScreen from './screens/webviewSample';
-
-const Tab = createBottomTabNavigator();
+import TabNavigator from './TabNavigator';
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen
-          options={{headerShown: false}}
-          name="WebViewSample"
-          component={WebViewSampleScreen}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  return <TabNavigator />;
 };
 
 export default App;
--- packages/santa_close_common/src/index.ts
+++ packages/santa_close_common/src/index.ts
@@ -1,2 +1,3 @@
 export * from './apollo'
+export * from './bridge'
 export * from './graphql'
