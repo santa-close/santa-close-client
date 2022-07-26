ab1ae898a124acd4081f96fc58c7ae3587ef957f
Seongcheol Jo <josworks27@gmail.com>
2022-07-26 22:22:44 +0900

    update: kakao login(wip) - 훅 분리

--- packages/santa_close_frame/src/App.tsx
+++ packages/santa_close_frame/src/App.tsx
@@ -1,82 +1,11 @@
-import React, {ChangeEvent, useEffect} from 'react'
+import React, {ChangeEvent} from 'react'
 import {RecoilRoot, useRecoilState} from 'recoil'
 import {sampleState} from 'map_app/atoms'
-import {UrqlProvider, urqlClient} from 'santa_close_common'
+import {UrqlProvider} from 'santa_close_common'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import gql from 'graphql-tag'
+import Login from './Login'
 
 const MapApp = React.lazy(() => import('map_app/MapApp'))
-
-const Login = () => {
-  const handleKaKaoLoginClick = () => {
-    Kakao.Auth.login({
-      success({access_token}) {
-        const foo = async () => {
-          const result = await urqlClient
-            .mutation(
-              gql`
-                mutation SignIn($input: SignInAppInput!) {
-                  signIn(input: $input) {
-                    accessToken
-                    expiredAt
-                  }
-                }
-              `,
-              {
-                input: {
-                  code: access_token,
-                  type: 'KAKAO',
-                },
-              },
-            )
-            .toPromise()
-
-          localStorage.setItem('token', JSON.stringify(result.data.signIn))
-        }
-
-        foo()
-      },
-      fail(err) {
-        console.log('err', err)
-      },
-    })
-  }
-
-  useEffect(() => {
-    const initKakaoScript = async () => {
-      const scriptInitResult = new Promise((resolve, reject) => {
-        const script = document.createElement('script')
-        script.src = 'https://developers.kakao.com/sdk/js/kakao.js'
-        script.defer = true
-        document.head.appendChild(script)
-
-        script.addEventListener('load', () => {
-          resolve({isLoaded: true, isError: false})
-        })
-
-        script.addEventListener('error', () => {
-          // eslint-disable-next-line prefer-promise-reject-errors
-          reject({isLoaded: false, isError: true})
-        })
-      })
-
-      await scriptInitResult
-
-      Kakao.init('1a01c6b9fba660692d7388f0f7c5baaf')
-      console.log(Kakao.isInitialized())
-    }
-
-    initKakaoScript()
-  }, [])
-
-  return (
-    <div>
-      <button type="button" onClick={handleKaKaoLoginClick}>
-        login
-      </button>
-    </div>
-  )
-}
 
 const MapAppContainer = () => {
   const [state, setState] = useRecoilState<string>(sampleState)
